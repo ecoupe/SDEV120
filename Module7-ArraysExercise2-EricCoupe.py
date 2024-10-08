59470a75e61fb5d87cefb9c62c7a87198134c2ba
Eric Coupe <118149003+ecoupe@users.noreply.github.com>
2024-10-07 23:24:26 -0400

    Add files via upload

--- Module7-ArraysExercise2-EricCoupe.py
+++ Module7-ArraysExercise2-EricCoupe.py
@@ -1,24 +1,24 @@
-addIns = ["whipped cream", "cinnamon", "chocolate sauce", "amaretto", "irish whiskey"]
+addIns = ["whipped cream", "cinnamon", "chocolate sauce", "amaretto", "irish whiskey"] #two seperate arrays
 prices = [0.89, 0.25, 0.59, 1.50, 1.75]
 baseCoffeePrice = 2.00
 totalPrice = baseCoffeePrice
 
-def availableMenu():
+def availableMenu():    #function to display a menu for the user
     print("\nWelcome to Jumpin' Jive Coffee Shop")
     print(f"\nOur base price for coffee is ${baseCoffeePrice}")
     print("Our available add-ins are:")
     for i in range(len(addIns)):
-        print(f"\n{addIns[i]} - ${prices[i]:.2f}")
+        print(f"\n{addIns[i]} - ${prices[i]:.2f}")  #format output as float with 2 decimal points
 
-availableMenu()
+availableMenu() #execute function
 
-while True:
+while True: #main processing loop. open for continuous input
     userAddIn = input("\nEnter an add-in (or type 'done' to complete the order): ")
 
-    if userAddIn == 'done':
+    if userAddIn == 'done': #sentinel value
         break
 
-    if userAddIn in addIns:
+    if userAddIn in addIns: #this for loop helps find the location of the user input within the arrays
         index = addIns.index(userAddIn)
         addInPrice = prices[index]
         totalPrice += addInPrice
