bd453efa678798b68eab313c3a5314dbaeeb8d07
Eric Coupe <118149003+ecoupe@users.noreply.github.com>
2024-10-10 13:23:47 -0400

    Add files via upload

---
+++ Module8-assignment-functions-in-python-EricCoupe.py
@@ -0,0 +1,13 @@
+def greaterThan (x,y):
+    if x > y:
+        return True
+    else:
+        return False
+
+varA = 10
+varB = 6
+
+result = greaterThan(varA, varB)
+
+print(f"The statement {varA} is greater than {varB} is {result}.")
+
